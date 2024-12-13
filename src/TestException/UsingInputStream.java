b10a1a90b6f4dd07dcd1cc471cd55789b17ff1e8
Cagmat <cagliesi.matteo@gmail.com>
2024-12-13 09:25:01 +0100

    primo commit

---
+++ src/TestException/UsingInputStream.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestException;
+
+/**
+ *
+ * @author B045_05
+ */
+public class UsingInputStream {
+        public static void usingScanner(){
+            Scanner in = new Scanner(System.in);
+		System.out.println("Digita un intero:");
+		
+		String line = in.next();
+		int intValue = Integer.parseInt(line);
+	    
+		System.out.println("Valore: " + intValue);
+		in.close();
+        }
+}
