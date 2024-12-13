5693b3d6847bd74f549b7fe4f9ce9aec849aad03
Cagmat <cagliesi.matteo@gmail.com>
2024-12-13 09:37:43 +0100

    13/12 esercizio

--- src/TestException/UsingInputStream.java
+++ src/TestException/UsingInputStream.java
@@ -4,6 +4,9 @@
  */
 package TestException;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author B045_05
@@ -11,12 +14,17 @@ package TestException;
 public class UsingInputStream {
         public static void usingScanner(){
             Scanner in = new Scanner(System.in);
-		System.out.println("Digita un intero:");
-		
-		String line = in.next();
-		int intValue = Integer.parseInt(line);
-	    
-		System.out.println("Valore: " + intValue);
-		in.close();
+            System.out.println("Digita un intero:");
+            try{
+                String line = in.next();
+                int intValue = Integer.parseInt(line);
+                System.out.println("Valore: " + intValue);
+            } catch(NumberFormatException e){
+                System.err.println("Non hai inserito un valore numerico!!");
+            } catch (InputMismatchException e) {
+			System.out.println("Exception message: " + e.getMessage());
+            }finally{
+                in.close();
+            }
         }
 }
